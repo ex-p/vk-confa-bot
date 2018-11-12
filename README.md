ce947f3a1e6a1f58a258d0413adf70e0062cd38f
alkapov <ralkapov@gmail.com>
2018-11-12 19:03:16 +0500

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+```
+https://oauth.vk.com/authorize?client_id=6730456&scope=messages,offline&redirect_uri=https://oauth.vk.com/blank.html&display=page&v=5.87&response_type=token 
+```
--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
-import atexit
-import copy
-import threading
-import os
+import logging
 import queue
+import threading
 
 import attr
+import click
 import requests
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 def get(arr, idx, default=None):
@@ -21,7 +24,7 @@ class Update(object):
     @classmethod
     def from_dict(cls, data):
         code = data[0]
-        print(data)
+        logger.info(data)
         if code == 4:
             message_update = MessageUpdate(code=code,
                                            message_id=data[1],
@@ -29,6 +32,7 @@ class Update(object):
                                            peer_id=get(data, 3),
                                            timestamp=get(data, 4),
                                            text=get(data, 5),
+                                           extra=get(data, 6)
                                            )
         else:
             message_update = cls(code=code)
@@ -43,12 +47,36 @@ class MessageUpdate(Update):
     timestamp = attr.ib(default=None)
     subject = attr.ib(default=None)
     text = attr.ib(default=None)
+    extra = attr.ib(default=None)
 
     def is_chat(self):
         return self.peer_id > 2000000000
 
+    def chat_id(self):
+        return self.peer_id - 2000000000
+
     def is_outbox(self):
         return self.flags & 2 == 2
+
+    def sender(self):
+        if self.is_chat():
+            if self.extra:
+                sender = self.extra.get('from', None)
+                return int(sender) if sender else None
+            return None
+        return self.peer_id
+
+
+@attr.s
+class Attachment(object):
+    attachment_type = attr.ib()
+    owner_id = attr.ib()
+    media_id = attr.ib()
+
+    def to_string(self):
+        return '{}{}_{}'.format(self.attachment_type,
+                                self.owner_id,
+                                self.media_id)
 
 
 class VkApi(object):
@@ -58,7 +86,8 @@ class VkApi(object):
         self._access_token = access_token
 
     def _make_request(self, method_name, params):
-        url = f'{self.BASE_VK_API_URL}{method_name}'
+        url = '{}{}'.format(self.BASE_VK_API_URL, method_name)
+        logger.info('Sending {} with {}'.format(method_name, params))
         parameters = {'access_token': self._access_token}
         parameters.update(params)
         return requests.get(url, params=parameters)
@@ -84,7 +113,8 @@ class VkApi(object):
         response = self._make_request(method_name, params)
         return self._process_response(response, LongPollServer)
 
-    def get_long_poll_update(self, long_poll_server, mode=0):
+    @staticmethod
+    def get_long_poll_update(long_poll_server, mode=0):
         params = long_poll_server.to_dict()
         params.update({
             'act': 'a_check',
@@ -92,17 +122,78 @@ class VkApi(object):
             'mode': mode,
             'version': 2
         })
-        url = f'https://{long_poll_server.server}'
+        url = 'https://{}'.format(long_poll_server.server)
         response = requests.get(url, params=params)
         response.raise_for_status()
         data = response.json()
         return data['ts'], [Update.from_dict(u) for u in data['updates']]
 
-    def send(self, peer_id, message, version='5.38'):
+    def delete(self, *message_ids, version='5.87'):
+        method_name = 'messages.delete'
+        message_ids = ','.join(map(str, message_ids))
+        params = {
+            'message_ids': message_ids,
+            'v': version,
+        }
+        response = self._make_request(method_name, params)
+        return self._process_response(response, None)
+
+    def send(self, peer_id, message=None, *attachments, version='5.87'):
         method_name = 'messages.send'
         params = {
             'peer_id': peer_id,
-            'message': message,
+            'v': version,
+        }
+        if message:
+            params['message'] = message
+        attachments = ','.join(a.to_string() for a in attachments)
+        if attachments:
+            params['attachment'] = attachments
+        logger.info('Sending: {}'.format(params))
+        response = self._make_request(method_name, params)
+        return self._process_response(response, None)
+
+    def create_poll(self,
+                    question,
+                    *answers,
+                    is_anonymous=False,
+                    is_multiple=False,
+                    end_date=0,
+                    owner_id=None,
+                    photo_id=None,
+                    background_id=None,
+                    version='5.87',
+                    ):
+        method_name = 'polls.create'
+        assert(len(answers) > 0 and len(answers) <= 10)
+        answers = ('"{}"'.format(answer) for answer in answers)
+        # TODO: add params
+        params = {
+            'question': question,
+            'add_answers': '[{}]'.format(','.join(answers)),
+            'v': version,
+        }
+        response = self._make_request(method_name, params)
+        return self._process_response(response, None)
+
+    def get_users(self, *users_ids, fields=None, name_case=None,
+                  version='5.87'):
+        fields = fields or ['screen_name']
+        method_name = 'users.get'
+        params = {
+            'users_ids': ','.join(map(str, users_ids)),
+            'fields': ','.join(fields),
+            'v': version,
+        }
+        response = self._make_request(method_name, params)
+        return self._process_response(response, None)
+
+    def get_chat(self, *chat_ids, fields=None, name_case=None, version='5.87'):
+        fields = fields or ['screen_name']
+        method_name = 'messages.getChat'
+        params = {
+            'chat_ids': ','.join(map(str, chat_ids)),
+            'fields': ','.join(fields),
             'v': version,
         }
         response = self._make_request(method_name, params)
@@ -124,37 +215,79 @@ class LongPollServer(object):
         return attr.asdict(self)
 
 
-def message_handler(pool: queue.Queue, vkApi: VkApi):
+def create_gathering(vk_api, message):
+    all_ = True
+    name = 'Кто на сходку?'
+    args = message.text.split(' ')[1:]
+    if args:
+        name = ' '.join(args)
+
+    if all_:
+        chats, _ = vk_api.get_chat(message.chat_id())
+        chat = chats[0]
+        users = chat['users']
+        names = ('@' + user['screen_name'] for user in users)
+        text = '{} {}'.format(name, ' '.join(names))
+        logger.info(vk_api.send(message.peer_id, text))
+
+
+def handle_command(vk_api, message):
+    command = message.text[1:].split(' ')[0].lower()
+    if command == 'сходка':
+        create_gathering(vk_api, message)
+
+
+def message_handler(pool: queue.Queue, vk_api: VkApi):
     while True:
         message = pool.get()
         if message is None:
             break
         if not message.is_outbox():
-            print(vkApi.send(message.peer_id, message.text))
+            logger.info("Message '{}' from {}".format(message.text,
+                                                      message.sender()))
+            if message.is_chat() and message.text.startswith('/'):
+                handle_command(vk_api, message)
+            # poll1, _ = vk_api.create_poll('adf', '+', '-', '1')
+            # poll2, _ = vk_api.create_poll('dw', '+', '-', '1')
+            # attachment1 = Attachment('poll', poll1['owner_id'], poll1['id'])
+            # attachment2 = Attachment('poll', poll2['owner_id'], poll2['id'])
+            # logger.info(vk_api.send(message.peer_id,
+            #                         None,
+            #                         attachment1,
+            #                         attachment2,
+            #                         version='5.87'))
+
         pool.task_done()
 
 
-def main():
-    access_token = os.environ['ACCESS_TOKEN']
-
-    vkApi = VkApi(access_token)
-    long_poll_server, error = vkApi.get_long_poll_server()
-    if error is not None:
-        print(error)
-        return
-
-    messages_pool = queue.Queue(maxsize=100)
-
-    message_handler_thread = threading.Thread(target=message_handler,
-                                              args=[messages_pool, vkApi])
-    message_handler_thread.start()
+@click.command()
+@click.option('--access_token', envvar='ACCESS_TOKEN', required=True)
+def main(access_token):
+    access_token = access_token
 
     while True:
-        ts, updates = vkApi.get_long_poll_update(long_poll_server, 0)
-        for update in updates:
-            if isinstance(update, MessageUpdate):
-                messages_pool.put(update)
-        long_poll_server.ts = ts
+        try:
+            vk_api = VkApi(access_token)
+            long_poll_server, error = vk_api.get_long_poll_server()
+            if error is not None:
+                logger.warning(error)
+                return
+
+            messages_pool = queue.Queue(maxsize=100)
+
+            message_handler_thread = threading.Thread(target=message_handler,
+                                                      args=[messages_pool,
+                                                            vk_api])
+            message_handler_thread.start()
+
+            while True:
+                ts, updates = vk_api.get_long_poll_update(long_poll_server, 2)
+                for update in updates:
+                    if isinstance(update, MessageUpdate):
+                        messages_pool.put(update)
+                long_poll_server.ts = ts
+        except Exception as e:
+            logging.exception(e)
 
 
 if __name__ == '__main__':
